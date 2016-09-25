ef30efff3520e20eab4b4a8b40eed4efffee6883
Omar Baradei <omarbaradei@uga.edu>
2016-09-25 16:26:56 -0400

    Tidying up code!

--- src/SearchTweets.java
+++ src/SearchTweets.java
@@ -6,26 +6,7 @@ import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 import java.util.List;
 
-/*
- * Copyright 2007 Yusuke Yamamoto
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
-/**
- * @author Yusuke Yamamoto - yusuke at mac.com
- * @since Twitter4J 2.1.7
- */
 public class SearchTweets {
 
         private static Twitter twitter;
@@ -41,16 +22,15 @@ public class SearchTweets {
         twitter = tf.getInstance();
     }
 
-
     /**
-     * Usage: java twitter4j.examples.search.SearchTweets [query]
+     * Temp
      *
      * @param args search query
      */
     public static void main(String[] args) {
         setupTwitter();
         if (args.length < 1) {
-            System.out.println("java twitter4j.examples.search.SearchTweets [query]");
+            System.out.println("No params inserted.");
             System.exit(-1);
         }
         try {
